20c02db5045e321694307f22cd75596267e5dd71
Antoine Guérette <antoine.guerette@gmail.com>
2023-02-27 18:21:00 -0500

    Civ setup should be done

---
+++ civ/scripts/data/abilities.js
@@ -0,0 +1,22 @@
+let abilities = [
+	{
+		name: 'TBA',
+		desc: 'To be added.',
+	},
+	{
+		name: 'Manifest Destiny',
+		desc: 'All land military units have +1 sight. 50% discount when purchasing tiles.',
+	},
+	{
+		name: 'Founding Father',
+		desc: 'TBA',
+	},
+	{
+		name: 'Emancipation Proclamation',
+		desc: 'Bonus for factories. Penalty for plantations.',
+	},
+	{
+		name: 'Corps of Discovery',
+		desc: 'Bonus to exploration.',
+	},
+];
